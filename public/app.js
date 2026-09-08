776ef4e873c56ae02a2e4b6ee6c0b5bca8ad9603
nadiaastramusic <nadia.astramusic@gmail.com>
2026-09-08 13:37:43 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -2,8 +2,11 @@
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 // Performance: cache static assets and API responses
