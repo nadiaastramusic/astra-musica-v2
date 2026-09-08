56f8913431051aabf284c60becabbcf52aba13b0
nadiaastramusic <nadia.astramusic@gmail.com>
2026-09-08 17:05:21 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -21,7 +21,6 @@ function downloadExcel(weekId) {
 }
 
 // ===================== IMPORTS & SETUP =====================
-const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
