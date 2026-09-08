1d646f2915df9f79db8f6dbe3f2da5a14ad18c14
nadiaastramusic <nadia.astramusic@gmail.com>
2026-09-08 12:27:31 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 // CORS
-app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
