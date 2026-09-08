f5731664f7ce64cefd55e907482ab887f82c28e1
nadiaastramusic <nadia.astramusic@gmail.com>
2026-09-08 17:13:10 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -21,8 +21,8 @@ function downloadExcel(weekId) {
 }
 
 // ===================== IMPORTS & SETUP =====================
+
 // CORS
-app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
